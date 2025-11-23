ff1d4e90b6f2f35db81694a4605f478bd853de8b
TheTradeVisor <admin@thetradevisor.com>
2025-11-23 12:44:30 +0000

    Fix: Use x-cloak instead of inline style for collapsible rows

--- resources/views/leaderboard/index.blade.php
+++ resources/views/leaderboard/index.blade.php
@@ -231,14 +231,14 @@
                                     {{-- Expandable Accounts Sub-table --}}
                                     @if($trader['account_count'] > 1)
                                         <tr x-show="expanded" 
+                                            x-cloak
                                             x-transition:enter="transition ease-out duration-200"
                                             x-transition:enter-start="opacity-0 transform scale-95"
                                             x-transition:enter-end="opacity-100 transform scale-100"
                                             x-transition:leave="transition ease-in duration-150"
                                             x-transition:leave-start="opacity-100 transform scale-100"
                                             x-transition:leave-end="opacity-0 transform scale-95"
-                                            class="bg-gray-50" 
-                                            style="display: none;">
+                                            class="bg-gray-50">
                                             <td colspan="8" class="px-6 py-4">
                                                 <div class="bg-white rounded-lg shadow-sm overflow-hidden">
                                                     <table class="min-w-full">
