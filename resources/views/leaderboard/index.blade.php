4cb81d2971c19b68422fbf42ce068ca7c426cd87
TheTradeVisor <admin@thetradevisor.com>
2025-11-23 12:29:29 +0000

    Fix leaderboard layout to use full width on desktop

--- resources/views/leaderboard/index.blade.php
+++ resources/views/leaderboard/index.blade.php
@@ -1,4 +1,19 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Top Traders Leaderboard | {{ config('app.name') }}</title>
+    
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-50">
         
         {{-- Simple Navigation --}}
@@ -221,4 +236,5 @@
         </div>
     </div>
     </div>
-</x-guest-layout>
+</body>
+</html>
