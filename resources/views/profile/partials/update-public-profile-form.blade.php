0114460a49c5e8d4e024f85d81f63d52aff06504
TheTradeVisor <admin@thetradevisor.com>
2025-11-23 11:14:58 +0000

    Fix: Remove parentheses from x-data attribute for Alpine.data() component

--- resources/views/profile/partials/update-public-profile-form.blade.php
+++ resources/views/profile/partials/update-public-profile-form.blade.php
@@ -9,7 +9,7 @@
         </p>
     </header>
 
-    <form method="post" action="{{ route('profile.public.update') }}" class="mt-6 space-y-6" x-data="publicProfileForm()" @submit.prevent="handleSubmit">
+    <form method="post" action="{{ route('profile.public.update') }}" class="mt-6 space-y-6" x-data="publicProfileForm" @submit.prevent="handleSubmit">
         @csrf
         @method('patch')
 
